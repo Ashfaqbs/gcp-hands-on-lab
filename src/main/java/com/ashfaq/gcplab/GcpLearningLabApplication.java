ba4b146f48ce421e3ae5eda40ce784b1d08be712
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2026-08-31 16:18:04 +0530

    chore: initial Spring Boot 4 / Java 17 project scaffold
    Base project generated via start.spring.io, Maven wrapper included.
    No GCP dependencies yet - added per-module as each service is explored.

---
+++ src/main/java/com/ashfaq/gcplab/GcpLearningLabApplication.java
@@ -0,0 +1,13 @@
+package com.ashfaq.gcplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpLearningLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpLearningLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashfaq/gcplab/GcpLearningLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashfaq.gcplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpLearningLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
